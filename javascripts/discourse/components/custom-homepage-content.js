7c80f6c28c049630ad32d12fcadb1925661c2ef8
mihirranjan7 <mihirranjan730@gmail.com>
2025-06-15 11:56:08 +0530

    Update custom-homepage-content.js

--- javascripts/discourse/components/custom-homepage-content.js
+++ javascripts/discourse/components/custom-homepage-content.js
@@ -1,12 +1,32 @@
 import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
 import { service } from "@ember/service";
+import { ajax } from "discourse/lib/ajax";
 import { defaultHomepage } from "discourse/lib/utilities";
 
 export default class CustomHomepageContent extends Component {
   @service router;
+  @tracked topUsers = [];
+
+  constructor() {
+    super(...arguments);
+    this.loadLeaderboard();
+  }
 
   get isHomepage() {
     const { currentRouteName } = this.router;
     return currentRouteName === `discovery.${defaultHomepage()}`;
+  }
+
+  @action
+  async loadLeaderboard() {
+    // Adjust order/period as needed
+    const data = await ajax("/directory_items.json?order=likes_received&period=weekly");
+    // Only take top 10 users and filter out staged or anonymized
+    this.topUsers = data.directory_items
+      .filter(item => !item.user.staged && !item.user.anonymized)
+      .slice(0, 10)
+      .map(item => item.user);
   }
 }
